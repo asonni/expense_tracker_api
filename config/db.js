27a2fd2e3090eb66575ae453cf0c5be584362c53
Aladdin Sonni <aladdin.sonni@gmail.com>
2020-02-26 09:16:32 +0200

    Add async/await to db.js file

--- config/db.js
+++ config/db.js
@@ -1,14 +1,22 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-  });
-  // eslint-disable-next-line no-console
-  console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+    });
+    // eslint-disable-next-line no-console
+    console.log(
+      `MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold
+    );
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(`Error: ${error.message}.red`);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
